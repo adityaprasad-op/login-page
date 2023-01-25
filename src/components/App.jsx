0d1ba9ce35fa066ea18939998e5a64be494b82ec
adityaprasad-op <aditya.prasad2002@gmail.com>
2023-01-25 11:19:43 +0530

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useReducer } from "react";
+import Form from "./Form";
+
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">
+      <Form 
+        isRegistered={userIsRegistered}
+      />
+    </div>
+  );
+}
+
+export default App;
