1f6ede7b89cdd39ce7fab5f5b1b5aee29d8a8c7c
adityaprasad-op <aditya.prasad2002@gmail.com>
2023-01-26 03:28:48 +0530

    replaced if else with spread operator

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,24 +15,9 @@ function Form(props) {
     const {name,value}= event.target;
     
     setContact((prevValue)=>{
-      if(name==="fName"){
-        return{
-          fName:value,
-          lName:prevValue.lName,
-          email:prevValue.email
-        }    
-      }else if(name==="lName"){
-      return{
-        fName:prevValue.fName,
-        lName:value,
-        email:prevValue.email
-      }     
-      }else if(name==="email"){
-        return{
-          fName:prevValue.fName,
-          lName:prevValue.lName,
-          email:value
-        }                  
+      return{         //spread operator instead of if else used earlier
+        ...prevValue,
+      [name]:value
       }
     })
   }
